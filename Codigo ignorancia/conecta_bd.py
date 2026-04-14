5bc34ce6531d42090006245435a130fcb7b37d26
Uzziel Antonio Vargas Flores <uzzantonifvt@gmail.com>
2026-04-14 17:07:58 -0600

    Conecta BD sin errores de sintaxis

--- Codigo ignorancia/conecta_bd.py
+++ Codigo ignorancia/conecta_bd.py
@@ -1,20 +1,100 @@
 import pymysql
 
+##Procedimiento para conectar y extraer informacion de la BD
 def recupera_categoria():
-    conn = pymysql.connect(host='localhost', user ='root', passwd="", db='ignorancia')
-    cursor = conn.cursor()
-    cursor.execute('select descripcion from categoria')
-    categorias = cursor.fetchall()
-    conn.close
-    return categorias
+	##Se crea un objeto de coneccion a la BD
+	conn = pymysql.connect(host='localhost', user='root', passwd='', db='ignorancia')
+	##Se crea un cursor para ejecutar consultas a la base de datos
+	cursor = conn.cursor()
+	##Se utiliza el cursor para ejecutar la consulta sobre la tabla de categorias
+	cursor.execute('select descripcion from categoria')
+	##Se crea una lista para contener las categorias extraidas de la base de datos
+	categorias = cursor.fetchall()
+	##cerrar la base de datos
+	conn.close()
+	#print(categorias)
+	return categorias
 
-def recupera_preguntas (cat):
-    conn = pymysql.connect(host='localhost', user ='root', passwd='', db='ignorancia')
-    cursor = conn.cursor()
-    consulta='select b.id_pregunta,b.pregunta,b.opcion_1,b.opcion_2,b.opcion_3,b.opcion_4,b.correcto,b.id_pregunta '
-    consulta=consulta+'from categoria a, pregunta b '
-    consulta=consulta+' where a.descripcion="'+cat+'" and b.id_categoria=a.id_categoria '
-    cursor.execute(consulta)
-    preguntas= cursor.fetchall()
-    conn.close()
-    return preguntas    
+def recupera_preguntas(cat):
+	conn = pymysql.connect(host='localhost', user='root', passwd='', db='ignorancia')
+	cursor = conn.cursor()
+	consulta='select b.id_pregunta,b.pregunta,b.opcion1,b.opcion2,b.opcion3,b.opcion4,b.correcto,b.id_categoria '
+	consulta=consulta+' from categoria a, pregunta b'
+	consulta=consulta+' where a.descripcion="'+cat+'" and b.id_categoria=a.id_categoria '
+	cursor.execute(consulta)
+	preguntas = cursor.fetchall()
+	conn.close()
+	return preguntas
+
+def tabla_categoria(id):
+	conn = pymysql.connect(host='localhost', user='root', passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('select id_categoria,pregunta,opcion1,opcion2,opcion3,opcion4,correcto,id_categoria from pregunta where id_categoria=%s',(id))
+	cats= cursor.fetchall()
+	conn.close()
+	return cats
+
+def inserta_categoria(datos,id):
+	conn = pymysql.connect(host='localhost', user='root', passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('inserta into pregunta (pregunta,opcion1,opcion2,opcion3,opcion4,correcto,id_categoria) values(%s,%s,%s,%s,%s,%s,%s)', 
+	    (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5],id))
+	conn.commit()
+	conn.close()
+
+def borra_categoria(ab):
+	conn = pymysql.connect(host='localhost',user='root',passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('delete from categoria where Id_categoria=%s' (ab))
+	conn.commit()
+	conn.close()
+
+def selec_categoria(ab):
+	conn = pymysql.connect(host='localhost',user='root',passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('select id_categoria,pregunta,opcion1,opcion2,opcion3,opcion4,correcto,id_categoria from pregunta where id_categoria=%s',(id))
+	dato=cursor.fetchone()
+	return dato
+
+def modif_categoria(ab,descripcion):
+	conn = pymysql.connect(host='localhost',user='root',passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('select id_categoria, descripcion from categoria where id_categoria=%s',(ab))
+	conn.commit()
+	conn.close()
+
+def tabla_pregunta(id):
+	conn = pymysql.connect(host='localhost',user='root',passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('select id_categoria,pregunta,opcion1,opcion2,opcion3,opcion4,correcto,id_categoria from pregunta where id_categoria=%s',(id))
+	preguntas = cursor.fetchall()
+	conn.close()
+	return preguntas
+
+
+def selec_pregunta(ab):
+	conn = pymysql.connect(host='localhost',user='root',passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('select id_categoria,pregunta,opcion1,opcion2,opcion3,opcion4,correcto,id_categoria from pregunta where id_pregunta=%s',(ab))
+	dato=cursor.fetchone()
+	return dato
+
+
+def modif_pregunta(ab,datos):
+	conn = pymysql.connect(host='localhost',user='root',passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('update pregunta set pregunta=%s,opcion1=%s,opcion2=%s,opcion3=%s,opcion4=%s,correcto=%s where Id_pregunta=%s',
+    (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5],ab))
+	conn.commit()
+
+def borra_pregunta(ab):
+	conn = pymysql.connect(host='localhost',user='root',passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('delete from pregunta where Id_pregunta=%s' (ab))
+	conn.commit()
+
+def inserta_pregunta(datos,id):
+	conn = pymysql.connect(host='localhost',user='root',passwd='', db='ignorancia2')
+	cursor = conn.cursor()
+	cursor.execute('inserta into pregunta (pregunta,opcion1,opcion2,opcion3,opcion4,correcto,id_categoria) values(%s,%s,%s,%s,%s,%s,%s)', (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5],id))
+	conn.commit()
